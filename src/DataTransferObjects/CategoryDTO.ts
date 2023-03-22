4d7e063dacce7545d43a087cd6e808084c72f440
João Macêdo <joaomacedx@gmail.com>
2023-03-22 18:22:55 -0300

    feat: correction

--- src/DataTransferObjects/CategoryDTO.ts
+++ src/DataTransferObjects/CategoryDTO.ts
@@ -1,7 +1,13 @@
 
 class CategoryDTO {
-     name: string;
-     description: string; 
-}
+   public name: string;
+   public description: string; 
+   constructor(
+     _name: string,
+     _description: string){
+     this.name = _name;
+     this.description = _description;
 
+   }
+}
 export { CategoryDTO }
--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,22 +1,16 @@
 import { Router } from 'express';
 import { CategoryDTO } from '../DataTransferObjects/CategoryDTO';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/categories", (request, response)=> {
    const { name, description } = request.body;
-   const dto = new CategoryDTO();
-   dto.description = description;
-   dto.name = name;
-   const categoryAlreadyExists = categoriesRepository.findByName(dto);
-   if (categoryAlreadyExists){
-     return response.status(400).json({
-       error: "Category already exists"
-     });
-   }
-   categoriesRepository.create(dto);
+   const dto = new CategoryDTO(name, description);
+   const createCategoryService = new CreateCategoryService(categoriesRepository);
+   createCategoryService.build(dto);
    response.status(201).send();
 });
 
