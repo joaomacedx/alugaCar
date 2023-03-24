3faa6dbbbadb95534f0b3ea3ccad005e76452775
João Macêdo <joaomacedx@gmail.com>
2023-03-24 19:25:31 -0300

    Delete src/Factories directory

--- src/Factories/ICategoryFactory.ts
+++
@@ -1,8 +0,0 @@
-import { CategoryDTO } from "../DataTransferObjects/CategoryDTO";
-import { Category } from "../model/Category";
-
- interface ICategoryFactory{
-   build(dto: CategoryDTO) : Category;
- }
-
- export { ICategoryFactory }
