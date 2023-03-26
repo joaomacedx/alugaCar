90c0f6275cf60d93d7ae4fced4418e5a8a75646e
João Macêdo <joaomacedx@gmail.com>
2023-03-26 19:06:53 -0300

    feat: reading file and save a category

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,38 @@
-
+import fs from "fs";
+import { parse as csvParse } from "csv-parse";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import { ICategoryDTO } from "../../DataTransferObjects/ICategoryDTO";
+import { CategoryDTO } from "../../DataTransferObjects/CategoryDTO";
+import { ICategoryFactory } from "../../Factories/ICategoryFactory";
+import { CreateCategoryService } from "../../services/CreateCategoryService";
 class ImportCategoryUseCase{
-   public execute(file: any){
-     console.log(file);
+   constructor(
+     private createCategoryService: CreateCategoryService
+   ){}
+
+   public async execute(file: Express.Multer.File): Promise<void> {
+     const categories = await this.loadCategories(file);
+     for (let index = 0; index < categories.length; index++) {
+       let element = categories[index];
+       this.createCategoryService.execute(element);
+       }
+   }
+   
+   private async loadCategories(file: Express.Multer.File): Promise<CategoryDTO[]>{
+     return new Promise((resolve, reject )=>{
+       const stream = fs.createReadStream(file.path);
+       const categories: ICategoryDTO[] = [];
+       const parseFile = csvParse();
+       stream.pipe(parseFile);
+
+       parseFile.on("data", async (line)=> {
+
+        const[name, description] = line;
+        categories.push(new CategoryDTO(name, description));
+       }).on("end", ()=> {
+        resolve(categories);
+       });
+     });
    }
 }
  
