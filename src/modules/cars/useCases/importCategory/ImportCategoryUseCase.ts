f359e0e08550d6f2625dea2f1eec1efacba6524b
João Macêdo <joaomacedx@gmail.com>
2023-03-26 20:32:02 -0300

    docs: Creating application documentation

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -13,24 +13,23 @@ class ImportCategoryUseCase{
    public async execute(file: Express.Multer.File): Promise<void> {
      const categories = await this.loadCategories(file);
      for (let index = 0; index < categories.length; index++) {
-       let element = categories[index];
-       this.createCategoryService.execute(element);
+         let element = categories[index];
+         this.createCategoryService.execute(element);
        }
    }
    
    private async loadCategories(file: Express.Multer.File): Promise<CategoryDTO[]>{
-     return new Promise((resolve, reject )=>{
+     return new Promise((resolve, reject)=>{
        const stream = fs.createReadStream(file.path);
        const categories: ICategoryDTO[] = [];
        const parseFile = csvParse();
        stream.pipe(parseFile);
 
        parseFile.on("data", async (line)=> {
-
-        const[name, description] = line;
-        categories.push(new CategoryDTO(name, description));
+         const[name, description] = line;
+         categories.push(new CategoryDTO(name, description));
        }).on("end", ()=> {
-        resolve(categories);
+         resolve(categories);
        });
      });
    }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import  Express  from 'express';
 import routes from './routes/routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json'
 
 const app = Express();
 
 app.use(Express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 
 app.listen(8080, () => {
