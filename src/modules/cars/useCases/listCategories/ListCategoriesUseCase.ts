212d842b53592345a17e346f23981aa00614f70f
João Macêdo <joaomacedx@gmail.com>
2023-05-01 09:45:53 -0300

    refactor: change ListCategories implementantion and ImportCategory, to meet requirements and follow project design standards

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,19 @@
+
+import { Request, Response } from "express";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import { inject, injectable } from "tsyringe";
+import { Category } from "../../entities/Category";
+
+@injectable()
+class ListCategoriesUseCase{
+   constructor(
+    @inject("CategoriesRepository")
+     private categoriesRepository: ICategoriesRepository
+   ){}
+   public  async execute(): Promise<Category[]> {
+     const all = await this.categoriesRepository.list();
+     return all;
+   }
+}
+export { ListCategoriesUseCase }
+
--- src/modules/cars/useCases/listCategories/index.ts
+++
@@ -1,7 +0,0 @@
-import { CategoriesRepository } from "../../repositories/implementations/InMemoryCategoriesRepository";
-import { ListCategoriesController } from "./ListCategoriesController";
-
-const categoriesRepository = CategoriesRepository.getInstance();
-const listCategoriesController = new ListCategoriesController(categoriesRepository);
-
-export { listCategoriesController }
