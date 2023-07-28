faa07ea9e3cd3e56b6d8646eeeb3c8925fc5d53b
João Macêdo <joaomacedx@gmail.com>
2023-07-28 09:00:10 -0300

    build: correction in import of ICreateCategoryFactory

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import { ICategoryDTO } from "../../DataTransferObjects/ICategoryDTO";
-import { ICategoryFactory } from "../../Factories/ICategoryFactory";
+import { ICategoryFactory } from "../../factories/ICategoryFactory";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 @injectable()
