92f5f579a9ff86cffa2f45d2bf03db44ae817a1d
João Macêdo <joaomacedx@gmail.com>
2023-09-08 08:09:26 -0300

    feat: add express-async-errors to emit errors for the user

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -4,12 +4,12 @@ import { Request, Response } from "express";
 import { CategoryDTO } from "../../dto/implementations/CategoryDTO";
 
 class CreateCategoryController{
-  public handle(request: Request, response: Response): Response {
+  public async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
     const dto = new CategoryDTO(name, description);
     try{
       const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
-      createCategoryUseCase.execute(dto);
+      await createCategoryUseCase.execute(dto);
       return response.status(201).send();
     } catch (error) {
       return response.status(400).json({error: error.message });
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import  Express, { NextFunction }  from 'express';
+import "express-async-errors";
 import routes from './routes/router';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
@@ -16,15 +17,20 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 
 app.use((error: Error, request: Request, response: Response, next: NextFunction ) => {
-    if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
-                message: error.message
-            });
-    }
-    return response.status(500)
-})
+  if (error instanceof AppError) {
+
+    return response.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error - ${error.message}`,
+  });
+});
 
 app.listen(8080, () => {
-    console.log("server is running");
+  console.log("Server is running");
 });
 
