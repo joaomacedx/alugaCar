3e7baf2cc4bcce1d744089614bfc30b00d8c613e
João Macêdo <joaomacedx@gmail.com>
2023-09-10 06:33:06 -0300

    feat: finish development of upload avatar use case

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarControler";
+import multer from "multer";
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+const uploadAvatar = multer(
+  uploadConfig.upload(
+    "./tmp/avatar",
+    ));
 
-usersRoutes.post("/", createUserController.handle)
+usersRoutes.post("/", createUserController.handle);
+usersRoutes.patch("/avatar",
+  uploadAvatar.single(
+    "avatar",
+  ),
+  ensureAuthenticated,
+  updateUserAvatarController.handle,
+  );
 
 export { usersRoutes }
